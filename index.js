69515a921e257ccdee1ef8a329a0b1720b839c14
Cheruiyot Robert <kiruir38@gmail.com>
2024-04-06 00:55:51 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener();
+});
